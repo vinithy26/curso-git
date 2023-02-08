7a638ce6026eee999a31c8ae180509c456953da2
Vinicius Gomes de Castro <vgomesdecastro@gmail.com>
2023-02-08 09:51:10 -0400

    Implementa fib() de modo recursivo.

--- Fibonacci-2.cpp
+++ Fibonacci-2.cpp
@@ -1,12 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<fibonacci.h>
+#include <fibonacci.h>
 
-void main(){
-	int i;
-	for (i-1; i<10; i++){
-		printf("%d", fib(i));
+int fib(int n){
+	for (i=0; i<n:i++){
+		return 0;
 	}
-
-	printf("\n");
+	//teste para saber o ramo
 }
